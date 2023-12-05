5d67e9996a15d2014aefc9cb00a9ce94d55ce5f7
pierre-mikelemelinerwood <pm.lemeliner@gmail.com>
2023-12-05 08:48:28 +0100

    simple search

--- index.ts
+++ index.ts
@@ -4,15 +4,15 @@ import * as fs from 'fs';
 import player from 'play-sound';
 
 const musicFolder: string = './music/';
-
-function searchMusicByArtist(artist?: string): void {
-    fs.readdir(musicFolder, (err: Error | null, files: string[]) => {
+const cliPlayer = player()
+async function searchMusicByArtist(artist?: string): Promise<void> {
+    fs.readdir(musicFolder, async (err: Error | null, files: string[]) => {
         if (err) {
             console.error('Error reading music folder:', err);
             return;
         }
         console.log(`Songs by ${artist || 'any artist'}:`);
-        const artistSongs = artist ? files.filter(file => file.includes(artist)) : files;
+        const artistSongs = artist ? files.filter(file => file.toLocaleLowerCase().includes(artist.toLocaleLowerCase())) : files;
         if (artistSongs.length === 0) {
             console.log(`No songs found for ${artist || 'any artist'}`);
             return;
@@ -20,18 +20,27 @@ function searchMusicByArtist(artist?: string): void {
         const randomIndex = Math.floor(Math.random() * artistSongs.length);
         const randomSong = artistSongs[randomIndex];
         console.log(randomSong);
-        playMusic(randomSong);
+        await playMusic(randomSong);
+        await searchMusicByArtist(artist)
     });
 }
 
-function playMusic(song: string): void {
+function playMusic(song: string): Promise<void> {
     const songPath: string = musicFolder + song;
     console.log(`Playing ${songPath}`);
-    player().play(songPath, (err: Error | null) => {
-        if (err) {
-            console.error('Error playing music:', err);
-        }
+    return new Promise((resolve, reject) => {
+        cliPlayer.play(songPath, (err: Error | null) => {
+            if (err) {
+                console.error('Error playing music:', err);
+                reject(err);
+            }
+            resolve();
+        })
     })
 }
+const artiste: string = process.argv[2]
 
-searchMusicByArtist();
+
+searchMusicByArtist(artiste);
+
+
