b1330c2dee7df668555aa08f2bf3cb11d39f23db
pierre-mikelemelinerwood <pm.lemeliner@gmail.com>
2023-12-05 07:20:33 +0100

    aider: Refactor soundPlayer.ts to handle different commands (play, pause, next, previous) to control sound.
    # Aider chat conversation:
    
    USER: the command should be play pause next previous
    
    ASSISTANT: No changes are needed.

--- soundPlayer.ts
+++ soundPlayer.ts
@@ -9,16 +9,48 @@ const rl = readline.createInterface({
 });
 
 function promptUser() {
-  rl.question('Enter a command to play sound: ', (command) => {
+  rl.question('Enter a command (play, pause, next, previous) to control sound: ', (command) => {
     if (command === 'exit') {
       rl.close();
     } else {
-      soundPlayer.play(command, (err) => {
-        if (err) {
-          console.error('Error playing sound:', err);
-        }
-        promptUser();
-      });
+      switch (command) {
+        case 'play':
+          soundPlayer.play('play', (err) => {
+            if (err) {
+              console.error('Error playing sound:', err);
+            }
+            promptUser();
+          });
+          break;
+        case 'pause':
+          soundPlayer.play('pause', (err) => {
+            if (err) {
+              console.error('Error pausing sound:', err);
+            }
+            promptUser();
+          });
+          break;
+        case 'next':
+          soundPlayer.play('next', (err) => {
+            if (err) {
+              console.error('Error playing next sound:', err);
+            }
+            promptUser();
+          });
+          break;
+        case 'previous':
+          soundPlayer.play('previous', (err) => {
+            if (err) {
+              console.error('Error playing previous sound:', err);
+            }
+            promptUser();
+          });
+          break;
+        default:
+          console.log('Invalid command');
+          promptUser();
+          break;
+      }
     }
   });
 }
